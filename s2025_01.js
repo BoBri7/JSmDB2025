7018b942ced0fcc0a74dede160aa2b167d1aea88
Bogdan <43040397+BoBri7@users.noreply.github.com>
2025-01-18 17:53:39 +0100

    Update s2025_01.js
    BugFx

--- s2025_01.js
+++ s2025_01.js
@@ -4,9 +4,9 @@ function h(){ let s ="s2025_01.js",d="\n"
   s+= d+"   format date"
   s+= d+"   format number"
   s+= d+"   format string "
-  s+= d+"+ .st 
+  s+= d+"+ .st "
   s+= d+"    list multi line sting "
-  s+= d+"    list bulet field        
+  s+= d+"    list bulet field "       
   s+= d+"+ specif?? "
   return s
 }
