b897a1e1d7875d3cdd178c1f0cd82ea0ea258f03
Bogdan <43040397+BoBri7@users.noreply.github.com>
2025-01-18 17:43:18 +0100

    Update s2025_01.js
    Spisek ToDo za GH 2025 repozitorij

--- s2025_01.js
+++ s2025_01.js
@@ -1,1 +1,12 @@
-s2025_01.js
+// s2025_01.js
+function h(){ let s ="s2025_01.js",d="\n"
+  s+= d+"+ jsField functions"    
+  s+= d+"   format date"
+  s+= d+"   format number"
+  s+= d+"   format string "
+  s+= d+"+ .st 
+  s+= d+"    list multi line sting "
+  s+= d+"    list bulet field        
+  s+= d+"+ specif?? "
+  return s
+}
